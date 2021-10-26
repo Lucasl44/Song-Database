0a77d5b3b4561388b62a9eb67b313bdd2adeaa27
Lucas Lederman <ljlederman@gmail.com>
2021-10-26 13:44:19 +0100

    Added more to the security

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ const express = require("express");
 const passport = require("passport");
 const app = express();
 
+const connection = require("./connection");
+const User = require("./models/user");
+const userRouter = require("./routes/user");
+
+app.use(express.json());
+app.use(passport.initialize());
+
+app.use("/users", userRouter);
+
+app.listen(process.env.HTTP_PORT || 80, () => {
+    connection.authenticate();
+    User.sync({alter: true});
+    console.log("App Online");
+});
