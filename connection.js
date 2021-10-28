39621349284d0786090eb48e99a859aae7c932c8
Lucas Lederman <89636072+Lucasl44@users.noreply.github.com>
2021-10-28 16:11:55 +0100

    Update connection.js

--- connection.js
+++ connection.js
@@ -5,7 +5,7 @@ let connection;
 if (process.env.NODE_ENV === "production") {
 
     module.exports = new Sequelize(`${process.env.DATABASE_URL}?sslmode=require`, {
-        url: process.env.DATABASE_URI,
+        url: process.env.DATABASE_URL,
         dialect: "postgres",
         dialectOptions: {
             ssl: {
